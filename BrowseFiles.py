632d28b302e468ed81b2b868694f353dd7d0734c
kits4 <khanolkarketan@gmail.com>
2021-01-24 21:08:20 +0530

    Made the window adjustable according to user's window.

---
+++ BrowseFiles.py
@@ -0,0 +1,17 @@
+from tkinter.filedialog import askopenfilename
+def browseFile(event):
+
+    filename = askopenfilename(filetypes=(("Jpeg images","*.jpg"),("PNG images","*.png"),("All Files","*.*")))
+    print(filename)
+    # root1=Toplevel()
+    # root1.wm_iconbitmap("icon.ico")
+    # lo = Image.open(filename)
+    # ren = ImageTk.PhotoImage(lo)
+    # width=ren.width()
+    # height=ren.height()
+    # resolution=str(width)+'x'+str(height)
+    # root1.geometry(resolution)
+    # img12 = Label(root1, image=ren)
+    # img12.image = ren
+    # img12.place(x=0, y=0)
+    # root1.mainloop()
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from tkinter import *
-from tkinter.filedialog import askopenfilename
 from tkinter import ttk
 from PIL import Image, ImageTk
+from BrowseFiles import browseFile
 
 root = Tk()
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+screen_resolution = "{}x{}+{}+{}".format(width, height, 0, 0)
 root.title("Face Swap")
-root.geometry('1600x1000')
+root.geometry(screen_resolution)
 root.wm_iconbitmap("icon.ico")
 
 load = Image.open("final.png")
+load = load.resize((width, height), Image.LANCZOS)
 render = ImageTk.PhotoImage(load)
 
 # labels can be text or images
@@ -16,21 +20,20 @@ img = Label(root, image=render)
 img.image = render
 img.place(x=0, y=0)
 
+# #new = Button(root, text="SNAPSHOT", activebackground="green", fg="white", bg="blue", width=15, height=3,relief='ridge',borderwidth=7)
+# #new.bind("<Button-1>",nw)
+# #new.place(x=670, y=400)
 
-#new = Button(root, text="SNAPSHOT", activebackground="green", fg="white", bg="blue", width=15, height=3,relief='ridge',borderwidth=7)
-#new.bind("<Button-1>",nw)
-#new.place(x=670, y=400)
+# app=Button(root,text="AVATARS",fg="white", bg="blue", width=15, height=3,relief='ridge',borderwidth=7)
+# app.bind("<Button-1>",charselect)
+# app.place(x=670,y=340)
 
-app=Button(root,text="AVATARS",fg="white", bg="blue", width=15, height=3,relief='ridge',borderwidth=7)
-app.bind("<Button-1>",charselect)
-app.place(x=670,y=340)
-
-view = Button(root, text="View", fg="white", bg="blue", width=15, height=3,relief='ridge',borderwidth=7)
-view.place(x=670, y=600)
+view = Button(root, text="View", fg="white", bg="blue", width=15, height=3, relief='ridge',borderwidth=7)
+view.place(x=400, y=300)
 view.bind("<Button-1>",browseFile)
 
-Filters = Button(root, text="Filters", fg="white", bg="blue", width=15, height=3,relief='ridge',borderwidth=7)
-Filters.place(x=670, y=470)
-Filters.bind("<Button-1>",lol33)
+# Filters = Button(root, text="Filters", fg="white", bg="blue", width=15, height=3,relief='ridge',borderwidth=7)
+# Filters.place(x=670, y=470)
+# Filters.bind("<Button-1>",lol33)
 
 root.mainloop()
